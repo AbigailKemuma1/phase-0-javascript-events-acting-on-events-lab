6213409a99f09049cf48898e8af3061c945b8a72
AbigailKemuma1 <abigail.kemuma@student.moringaschool.com>
2025-06-24 18:03:29 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+// Your code here//
+const dodger = document.getElementById('dodger')
+console.log (dodger);
+dodger.style.backgroundColor = "000000";
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "100px";
+dodger.style.bottom = "0px";
+dodger.style.left = "100px"
+document.addEventListener("keydown", function (event){
+    if (event.key === "ArrowLeft"){
+        moveDodgerLeft()
+    }
+    if (event.key === "ArrowRight"){
+        moveDodgerRight() 
+    }
+})
+function moveDodgerLeft(){   
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left > 0){
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "")
+    const left = parseInt(leftNumbers, 10)
+    if (left < 360){
+        dodger.style.left = `${left + 1}px`;
+    }
+};
+
